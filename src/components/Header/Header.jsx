3550211b2a3cdc801c6c7d1596415a12f84fd411
Henrique Barros <henrique1_barros@hotmail.com>
2022-06-06 19:59:17 -0300

    fix: header responsivity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,28 @@
-import './styles.css'
-import Logo from '../Logo/Logo'
-import InputSearch from '../InputSearch/InputSearch'
+import "./styles.css";
+import Logo from "../Logo/Logo";
+import InputSearch from "../InputSearch/InputSearch";
 
-function Header({searchItem, setSearchItem, filterCards, setFilterOn, handleChange}){
-    return(
-        <div className='header'>
-            <Logo></Logo>
-            <InputSearch searchItem={searchItem} setSearchItem={setSearchItem} filterCards={filterCards} setFilterOn={setFilterOn} handleChange={handleChange}></InputSearch>
-        </div>
-    )
+function Header({
+  searchItem,
+  setSearchItem,
+  filterCards,
+  setFilterOn,
+  handleChange,
+}) {
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <Logo />
+        <InputSearch
+          searchItem={searchItem}
+          setSearchItem={setSearchItem}
+          filterCards={filterCards}
+          setFilterOn={setFilterOn}
+          handleChange={handleChange}
+        ></InputSearch>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
