034b7fbcb7e3175e728defa94a8c9b83be37b9e9
Henrique Barros <henrique1_barros@hotmail.com>
2022-06-07 00:29:19 -0300

    fix: card margings

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,10 +7,14 @@ function Product({ product, handleClick }) {
         <img src={product.img} alt="."></img>
       </div>
       <div className="product-info">
-        <h3>{product.name}</h3>
-        <span>{product.category}</span>
-        <h5>R${product.price.toFixed(2).replace(".", ",")}</h5>
-        <button id={product.id} onClick={(evt) => handleClick(evt.target.id)}>Adicionar</button>
+        <div className="product-info--content">
+          <h3>{product.name}</h3>
+          <span>{product.category}</span>
+          <h5>R${product.price.toFixed(2).replace(".", ",")}</h5>
+          <button id={product.id} onClick={(evt) => handleClick(evt.target.id)}>
+            Adicionar
+          </button>
+        </div>
       </div>
     </div>
   );
